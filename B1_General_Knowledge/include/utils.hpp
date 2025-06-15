3d609b1b3454e6a6af6053cbbf625e4c0374aa0b
huy <huyanhh195@gmail.com>
2025-06-15 17:09:10 +0700

    Change the folder structure for General Knowledge

--- B1_General_Knowledge/include/utils.hpp
+++ B1_General_Knowledge/include/utils.hpp
@@ -3,11 +3,8 @@
 #include <csignal>
 #include <unistd.h>
 #include "my_logging.hpp"
-// #include "my_string.hpp"
 
 void singal_handler(int signal);
 
 int program_running = true;
 int NUMBER_COUNTS = 10;
-
-// MAIN_h
